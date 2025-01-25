a8c138c8395bdc07d9bfe0734a9af985130e6f50
Deepa <deepanshupatel9229@gmail.com>
2025-01-25 16:47:12 +0530

    play-pause

--- video.js
+++ video.js
@@ -1,8 +1,18 @@
  // disable rightclick
  document.addEventListener("contextmenu", (e)=>e.preventDefault());
 
+// Disable other keyboard shortcuts
  document.addEventListener("keydown",(e)=>{
     if(e.key == "F12")e.preventDefault();
     if(e.ctrlKey && e.shiftKey && e.key == "I")e.preventDefault();
     if(e.ctrlKey && e.key == "u")e.preventDefault();
  })
+
+// Get the video element inside the iframe
+const videoElement = document.querySelector('.video-container iframe')?.contentDocument?.querySelector('video');
+
+if (videoElement) {
+  videoElement.addEventListener('dblclick', (e) => {
+    e.preventDefault();
+  });
+}
